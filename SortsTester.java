ba18419ca5a40b17c292d104e33aea413162cf5b
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-29 10:49:25 -0500

    insertion and selectionSort tested,  I apologize for not repeatedly commiting, I take advantage of when the wifi crashes to do APCS HW

--- SortsTester.java
+++ SortsTester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class SortsTester{
   public static void main(String[] args){
-    int[] a = new int[]{-7,420,16,73,720,-218,623,803,14,1221,67,89,3,-2562,-382,718,843,145};
+    int[] a = new int[]{-7,420,16,720,-218,623,803,3,-2,-382,843,145};
     int[] b = new int[]{9,8,7,6,5,4,3,2,1,0};
     System.out.println(Arrays.toString(b));
     Sorts.bubbleSort(b);
@@ -16,5 +16,21 @@ public class SortsTester{
     int[] d =new int[0];
     Sorts.bubbleSort(d);
     System.out.println(Arrays.toString(d));
+
+    int[] e = new int[]{6,3,7,23,2,37,79,57,9,5,46,78};
+    Sorts.selectionSort(e);
+    System.out.println(Arrays.toString(e));
+
+    int[] f = new int[]{1,-3,5,-7,9,-11,13,15,17,-19,21};
+    Sorts.selectionSort(f);
+    System.out.println(Arrays.toString(f));
+    Sorts.selectionSort(d);
+    System.out.println(Arrays.toString(d));
+
+    int[] g = new int[]{22,-33,44,-55,66,-77,88,-99,111,-122};
+    System.out.println(Arrays.toString(g));
+    Sorts.insertionSort(g);
+    System.out.println(Arrays.toString(g));
+
   }
 }
