e469cfbc82701c302c6a27ce5a1fb8f09d560867
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-29 10:44:28 -0500

    insertion and selectionSort created, I apologize for not repeatedly commiting, I take advantage of when the wifi crashes to do APCS HW.

--- Sorts.java
+++ Sorts.java
@@ -17,4 +17,30 @@ public class Sorts{
       }
     }
   }
+
+  public static void selectionSort(int[] data){
+    int tempValue=218984276;
+    int tempIndex=0;
+    for(int i=0;i<data.length;i++){
+      for(int j=i;j<data.length;j++){
+        if(data[j]<tempValue){tempValue=data[j]; tempIndex=j;}
+      }
+      data[tempIndex]=data[i];
+      data[i]=tempValue;
+      tempValue=218984276;
+    }
+  }
+
+  public static void insertionSort(int[] data){
+    int tempV=0;
+    int temppV=0;
+    boolean notdone=true;
+    for(int i=1;i<data.length;i++){
+      tempV=data[i];
+      for(int j=i-1;j-1>=-1&&notdone;j--){
+        if(data[j]>tempV){temppV=data[j]; data[j]=tempV; data[j+1]=temppV;}else{notdone=false;}
+      }
+      notdone=true;
+    }
+  }
 }
